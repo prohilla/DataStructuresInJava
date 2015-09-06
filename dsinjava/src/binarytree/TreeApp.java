54a3b3de4c954aefa138340a1cbd0906f90cd348
prohilla <prashant643@gmail.com>
2015-09-05 20:33:11 -0700

    modified for traversal methods

--- dsinjava/src/binarytree/TreeApp.java
+++ dsinjava/src/binarytree/TreeApp.java
@@ -14,11 +14,23 @@ public class TreeApp {
 		
 		Node nodeFound=myTree.find(74);
 		if(nodeFound!=null){
-		System.out.println("Node found with key "+nodeFound.key+" and data "+nodeFound.data );
+			System.out.println("Search Results:");
+			System.out.println("Node found with key "+nodeFound.key+" and data "+nodeFound.data );
+			System.out.println();
 		}
 		else{
+			System.out.println("Search Results:");
 			System.out.println("Node with specified key not found");
 		}
+		
+		System.out.println("Start of Pre-Order traversal");
+		myTree.preOrderTraversal(myTree.root);
+		
+		System.out.println("Start of In-Order traversal");
+		myTree.inOrderTraversal(myTree.root);
+		
+		System.out.println("Start of Post-Order traversal");
+		myTree.postOrderTraversal(myTree.root);
 		myTree.delete(45);
 	}
 }
