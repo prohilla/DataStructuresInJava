8705931d269dd933bf2f7f5fd648a1e145b096f9
prohilla <prashant643@gmail.com>
2015-09-05 18:42:22 -0700

    Class representing tree root and operations

---
+++ dsinjava/src/binarytree/Tree.java
@@ -0,0 +1,18 @@
+package binarytree;
+
+public class Tree {
+	
+	private Node root;
+	
+	public void find(int key){
+		
+	}
+	
+	public void insert(int key, int data){
+		
+	}
+	
+	public void delete(int key){
+		
+	}
+}
