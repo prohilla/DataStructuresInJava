3b78a4cfd8c1881e427c61100c7785b1d8297682
prohilla <prashant643@gmail.com>
2015-09-05 18:35:10 -0700

    Class for representing node in a tree

---
+++ dsinjava/src/binarytree/Node.java
@@ -0,0 +1,13 @@
+package binarytree;
+
+public class Node {
+
+	int key;
+	int data;
+	Node leftChild;
+	Node rightChild;
+	
+	public void display(){
+		System.out.println("Node key :"+key+" | "+"Node data: "+data);
+	}
+}
